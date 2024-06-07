16847524c4c27a1b6f7e66df101c8e9e4f9e8c25
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-06 21:56:55 -0500

    nueva clase PRODUCTO con respectivos atributos

---
+++ src/main/java/org/jhonatan/app/Datos/Producto.java
@@ -0,0 +1,13 @@
+package org.jhonatan.app.Datos;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class Producto {
+    private int idProducto;
+    private String nombre;
+    private String descripcion;
+    private String unidadMedida;
+    private double precioVenta;
+}
