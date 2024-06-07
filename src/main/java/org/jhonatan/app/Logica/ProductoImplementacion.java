08ab11036813839f30ab153f870221cfc5f3c742
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-06 22:18:19 -0500

    clase PRODUCTO-IMPLEMENTACION: funcionalidad del método de mostrar productos en un jtable

--- src/main/java/org/jhonatan/app/Logica/ProductoImplementacion.java
+++ src/main/java/org/jhonatan/app/Logica/ProductoImplementacion.java
@@ -3,6 +3,7 @@ package org.jhonatan.app.Logica;
 import javax.swing.table.DefaultTableModel;
 import org.jhonatan.app.Datos.InterfacesDao.ProductoDao;
 import org.jhonatan.app.Datos.Producto;
+import java.sql.*;
 
 /**
  *
@@ -17,7 +18,37 @@ public class ProductoImplementacion implements ProductoDao {
 
     @Override
     public DefaultTableModel mostrarProductos(String buscar) {
+        Connection conex = conexion.conectarBD();
+        DefaultTableModel modelo;
+        String[] cabezera = {"ID", "PRODUCTO", "DESCRIPCIÓN", "UNIDAD DE MEDIDA", "PRECIO"};
+        String[] registros = new String[5];
+        totalRegistros = 0;
 
+        modelo = new DefaultTableModel(null, cabezera);
+
+        //consulta
+        sql = "SELECT * FROM producto WHERE nombre LIKE '%" + buscar + "%' ORDER BY idProducto";
+
+        try {
+            Statement st = conex.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            //recorremos los registros
+            while (rs.next()) {
+                registros[0] = rs.getString("idProducto");
+                registros[1] = rs.getString("nombre");
+                registros[2] = rs.getString("descripcion");
+                registros[3] = rs.getString("unidad_medida");
+                registros[4] = rs.getString("precio_venta");
+                totalRegistros++;
+                modelo.addRow(registros);
+            }
+            conexion.desconectarBD();
+            return modelo;
+        } catch (SQLException e) {
+            System.out.println("Error al mostrar datos de la tabla habitacion; " + e.toString());
+            return null;
+        }
     }
 
     @Override
