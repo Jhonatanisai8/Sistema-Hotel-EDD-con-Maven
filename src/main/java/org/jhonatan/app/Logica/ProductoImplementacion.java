e39b1beca716e209598a6906550ec89cecda7ce9
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-06 22:09:13 -0500

    clase PRODUCTO-IMPLEMENTACION:llamamos a todos los métodos de la interface anteriormente creada PRODUCTO-DAO

---
+++ src/main/java/org/jhonatan/app/Logica/ProductoImplementacion.java
@@ -0,0 +1,30 @@
+package org.jhonatan.app.Logica;
+
+import javax.swing.table.DefaultTableModel;
+import org.jhonatan.app.Datos.InterfacesDao.ProductoDao;
+import org.jhonatan.app.Datos.Producto;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class ProductoImplementacion implements ProductoDao {
+
+    @Override
+    public DefaultTableModel mostrarProductos(String buscar) {
+        
+    }
+
+    @Override
+    public void insertarProducto(Producto producto) {
+    }
+
+    @Override
+    public void modificarProducto(Producto producto) {
+    }
+
+    @Override
+    public void eliminarProducto(Producto producto) {
+    }
+
+}
