55aa5b7013f03f218b0ace2de874cc99a8c63528
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-08 13:43:40 -0500

    Clase cliente-imple: metodo para mostrar la lista de clientes

--- src/main/java/org/jhonatan/app/Logica/ClienteImple.java
+++ src/main/java/org/jhonatan/app/Logica/ClienteImple.java
@@ -1,5 +1,9 @@
 package org.jhonatan.app.Logica;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.table.DefaultTableModel;
 import org.jhonatan.app.Datos.Cliente;
 import org.jhonatan.app.Datos.InterfacesDao.ClienteDao;
@@ -10,24 +14,83 @@ import org.jhonatan.app.Datos.InterfacesDao.ClienteDao;
  */
 public class ClienteImple implements ClienteDao {
 
+    //instacion de la conexion;
+    Conexion conexion = Conexion.getInstancia();
+    private String sql = "";
+    private String sql2 = "";
+    public int totalRegistros;
+
     @Override
     public DefaultTableModel mostrarClientes(String buscar) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Connection conex = conexion.conectarBD();
+        DefaultTableModel modelo;
+        String[] cabezera = {"ID", "NOMBRE", "AP. PATERNO", "AP. MATERNO", "DOC", "Nº. DOC", "DIRECCIÓN", "TELÉFONO", "EMAIL", "CÓDIGO CLI"};
+        String[] registros = new String[10];
+        totalRegistros = 0;
+
+        modelo = new DefaultTableModel(null, cabezera);
+
+        //consulta
+        sql = "SELECT p.idPersona,"
+                + "p.nombre,"
+                + "p.apaterno,"
+                + "p.amaterno,"
+                + "p.tipo_documento,"
+                + "p.num_documento,"
+                + "p.direccion,"
+                + "p.telefono,"
+                + "p.email,"
+                + "c.codigo_cliente"
+                + " FROM persona p INNER JOIN cliente c "
+                + " ON p.idpersona = c.idpersona WHERE num_documento like '%" + buscar + "%' ORDER BY idpersona DESC";
+
+        try {
+            Statement st = conex.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            //recorremos los registros
+            while (rs.next()) {
+                registros[0] = rs.getString("idPersona");
+                registros[1] = rs.getString("nombre");
+                registros[2] = rs.getString("apaterno");
+                registros[3] = rs.getString("amaterno");
+                registros[4] = rs.getString("tipo_documento");
+                registros[5] = rs.getString("num_documento");
+                registros[6] = rs.getString("direccion");
+                registros[7] = rs.getString("telefono");
+                registros[8] = rs.getString("email");
+                registros[9] = rs.getString("codigo_cliente");
+
+                totalRegistros++;
+                modelo.addRow(registros);
+            }
+            conexion.desconectarBD();
+            return modelo;
+        } catch (SQLException e) {
+            System.out.println("Error al mostrar datos de la tabla producto; " + e.toString());
+            return null;
+        } finally {
+            try {
+                conexion.desconectarBD();
+            } catch (SQLException ex) {
+                System.out.println("Error la cerrar la conexion en el metodo mostra productos");
+            }
+        }
     }
 
     @Override
     public void insertarCliente(Cliente cliente) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
     }
 
     @Override
     public void modificarCliente(Cliente cliente) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
     }
 
     @Override
     public void eliminarCliente(Cliente cliente) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
     }
 
 }
