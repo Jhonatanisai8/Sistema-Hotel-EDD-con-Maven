25df5a6f7efc2fd7073a153c23481402068ee0b4
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-08 10:41:58 -0500

    Clase Persona: getters y setters

--- src/main/java/org/jhonatan/app/Datos/Persona.java
+++ src/main/java/org/jhonatan/app/Datos/Persona.java
@@ -32,4 +32,76 @@ public class Persona {
         this.email = email;
     }
 
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAppPaterno() {
+        return appPaterno;
+    }
+
+    public void setAppPaterno(String appPaterno) {
+        this.appPaterno = appPaterno;
+    }
+
+    public String getAppMaterno() {
+        return appMaterno;
+    }
+
+    public void setAppMaterno(String appMaterno) {
+        this.appMaterno = appMaterno;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
