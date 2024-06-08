7991b8f27d8e3fb000b13251ab2afd7bc43528e9
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-08 12:15:35 -0500

    Clase cliente-imple: esta clase ya se les dara funcionalidad a los metodos que tiene la interface cliente-Dao

---
+++ src/main/java/org/jhonatan/app/Logica/ClienteImple.java
@@ -0,0 +1,33 @@
+package org.jhonatan.app.Logica;
+
+import javax.swing.table.DefaultTableModel;
+import org.jhonatan.app.Datos.Cliente;
+import org.jhonatan.app.Datos.InterfacesDao.ClienteDao;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class ClienteImple implements ClienteDao {
+
+    @Override
+    public DefaultTableModel mostrarClientes(String buscar) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void insertarCliente(Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void modificarCliente(Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminarCliente(Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
