fc1fead1deb774a740924a68f967eeb3bdd416d3
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-21 15:40:36 -0500

    interface Trabajdor-Dao: metodos que luego implementaremos

---
+++ src/main/java/org/jhonatan/app/Datos/InterfacesDao/TrabajadorDao.java
@@ -0,0 +1,20 @@
+package org.jhonatan.app.Datos.InterfacesDao;
+
+import javax.swing.table.DefaultTableModel;
+import org.jhonatan.app.Datos.Trabajador;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public interface TrabajadorDao {
+    //métodos
+
+    public DefaultTableModel mostrarTrabajadores(String buscar);
+
+    public void insertarTrabajador(Trabajador trabajador);
+
+    public void modificarTrabajador(Trabajador trabajador);
+
+    public void eliminarTrabajador(Trabajador trabajador);
+}
