73f7b6baa58b108654949a090be0c04f34542e0b
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-21 15:42:27 -0500

    clase Trabajador-Imple: esta clase ya les dara funcionalidad a la interface Trabajador-Dao

---
+++ src/main/java/org/jhonatan/app/Logica/TrabajadorImple.java
@@ -0,0 +1,29 @@
+package org.jhonatan.app.Logica;
+
+import javax.swing.table.DefaultTableModel;
+import org.jhonatan.app.Datos.InterfacesDao.TrabajadorDao;
+import org.jhonatan.app.Datos.Trabajador;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class TrabajadorImple implements TrabajadorDao {
+
+    @Override
+    public DefaultTableModel mostrarTrabajadores(String buscar) {
+    }
+
+    @Override
+    public void insertarTrabajador(Trabajador trabajador) {
+    }
+
+    @Override
+    public void modificarTrabajador(Trabajador trabajador) {
+    }
+
+    @Override
+    public void eliminarTrabajador(Trabajador trabajador) {
+    }
+
+}
