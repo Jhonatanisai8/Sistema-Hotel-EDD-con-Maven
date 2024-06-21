49578c2d963d2a249b9e3bedf05c2f711c2372d0
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-21 15:37:08 -0500

    clase trabajador: atributos esta clase es hija de la clase persona

---
+++ src/main/java/org/jhonatan/app/Datos/Trabajador.java
@@ -0,0 +1,16 @@
+package org.jhonatan.app.Datos;
+
+/**
+ *
+ * @author Jhonatan
+ */
+//clase hija de la clase persona
+public class Trabajador extends Persona {
+
+    private double sueldo;
+    private String acceso;
+    private String login;
+    private String password;
+    private String estado;
+
+}
